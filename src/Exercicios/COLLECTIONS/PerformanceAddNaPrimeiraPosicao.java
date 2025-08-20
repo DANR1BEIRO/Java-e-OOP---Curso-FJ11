a644e879ce53d449fe0b146ab0692d8a0b1a88d1
DANR1BEIRO <danribeirodev@gmail.com>
2025-08-20 12:26:01 -0300

    test(collections): Cria teste de perfurmance para insercão no início de uma LinkedList e um ArrayList

--- src/Exercicios/COLLECTIONS/PerformanceAddNaPrimeiraPosicao.java
+++ src/Exercicios/COLLECTIONS/PerformanceAddNaPrimeiraPosicao.java
@@ -1,4 +1,24 @@
 package Exercicios.COLLECTIONS;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class PerformanceAddNaPrimeiraPosicao {
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+        long total = 300_000;
+
+        long inicioInsercao = System.currentTimeMillis();
+
+        for (int i = 0; i < total; i++) {
+            numeros.add(0, i);
+        }
+
+        long fimInsercao = System.currentTimeMillis();
+        long tempoInsercao = fimInsercao - inicioInsercao;
+        double tempoInsercaoEmSegundos = tempoInsercao / 1000.0;
+
+        System.out.printf("Tempo de insercão no início(ms): %d ms%n", tempoInsercao);
+        System.out.printf("Tempo de insercão no início(seg): %.3f s%n", tempoInsercaoEmSegundos);
+    }
 }
