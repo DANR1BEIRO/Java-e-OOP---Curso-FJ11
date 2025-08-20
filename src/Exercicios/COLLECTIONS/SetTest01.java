fac19ed4b763993af461b17937827369b292cba7
DANR1BEIRO <danribeirodev@gmail.com>
2025-08-20 12:22:56 -0300

    feat(collections): Demonstra a utilizacão de HashSet e Iterator

--- src/Exercicios/COLLECTIONS/SetTest01.java
+++ src/Exercicios/COLLECTIONS/SetTest01.java
@@ -1,4 +1,25 @@
 package Exercicios.COLLECTIONS;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class SetTest01 {
+    public static void main(String[] args) {
+        Set<String> nomes = new HashSet<>();
+
+        nomes.add("Kakaroto");
+        nomes.add("Bulma");
+        nomes.add("Videl");
+
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+
+        Iterator<String> i = nomes.iterator();
+        while (i.hasNext()) {
+            String palavra = i.next();
+            System.out.println(palavra);
+        }
+    }
 }
