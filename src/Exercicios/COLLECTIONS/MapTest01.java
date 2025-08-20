33d6b66d41c2f1351385f0ae2a3c4000a7e7faa5
DANR1BEIRO <danribeirodev@gmail.com>
2025-08-20 12:24:06 -0300

    feat(collections): Apresenta o uso de HashMap com chaves e valores

--- src/Exercicios/COLLECTIONS/MapTest01.java
+++ src/Exercicios/COLLECTIONS/MapTest01.java
@@ -1,4 +1,25 @@
 package Exercicios.COLLECTIONS;
 
+import Banco.model.conta.ContaCorrente;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapTest01 {
+    public static void main(String[] args) {
+        ContaCorrente c1 = new ContaCorrente(1, 1000);
+        ContaCorrente c2 = new ContaCorrente(2, 2000);
+
+        Map<String, ContaCorrente> mapDeContas = new HashMap<>();
+        mapDeContas.put("Gerente", c1);
+        mapDeContas.put("Presidente", c2);
+
+        ContaCorrente gerente = mapDeContas.get("Gerente");
+        System.out.println("saldo do gerente: " + gerente.getSaldo());
+        System.out.println(mapDeContas.containsKey("Presidente"));
+        System.out.println(mapDeContas.containsValue(c1));
+        System.out.println(mapDeContas.keySet());
+        System.out.println(mapDeContas.values());
+
+    }
 }
