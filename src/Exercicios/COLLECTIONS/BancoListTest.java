b76de31e273f475ed218bf678e579a28d1c61889
DANR1BEIRO <danribeirodev@gmail.com>
2025-08-21 13:19:11 -0300

    feat(banco): Implementa a classe Banco e adiciona teste de colecão
    - Cria a classe 'Banco' para gerenciar um conjunto de contas, utilizando uma List
    para armazenamento e um Map para busca otimizada por nome de titular.
    - Adiciona um teste no método 'main' para validar o comportamento de unicidade da
    classe 'Conta' em um HashSet, confirmando a correta implementacão de equals/hashCode.

--- src/Exercicios/COLLECTIONS/BancoListTest.java
+++ src/Exercicios/COLLECTIONS/BancoListTest.java
@@ -1,4 +1,52 @@
 package Exercicios.COLLECTIONS;
 
+import Banco.model.conta.Conta;
+import Banco.model.conta.ContaCorrente;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+class Banco {
+    private List<Conta> contas;
+    private Map<String, Conta> dicionario = new HashMap<>();
+
+    public void adiciona(Conta conta) {
+        contas.add(conta);
+        if (!dicionario.containsKey(conta.getTitular().getNome())) {
+            dicionario.put(conta.getTitular().getNome(), conta);
+        }
+    }
+
+    public Conta getByIndex(int x) {
+        try {
+            return contas.get(x);
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Conta getByName(String name) {
+        return dicionario.get(name);
+    }
+
+    public int getQuantidadeDeContas() {
+        return contas.size();
+    }
+}
+
 public class BancoListTest {
+    public static void main(String[] args) {
+
+        HashSet<Conta> contaHashSet = new HashSet<>();
+        ContaCorrente c1 = new ContaCorrente(1, 200);
+        ContaCorrente c2 = new ContaCorrente(1, 200);
+
+        contaHashSet.add(c1);
+        contaHashSet.add(c2);
+        System.out.println(contaHashSet.size());
+
+    }
 }
